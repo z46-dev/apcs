f0d5cca145de22677dfaf4e26d2846ac8ebfdb35
z46-dev <evanparker6023@gmail.com>
2023-10-25 14:05:18 -0400

    Update LIFE

--- Chapter10/Life.java
+++ Chapter10/Life.java
@@ -18,10 +18,13 @@ public class Life {
     final private static int HEIGHT = 20;
     final private static int CUBE_SIZE = 40;
 
+    private static boolean tickBegan;
+
     public static void main(String[] args) {
         input = new Scanner(System.in);
         board = new boolean[WIDTH * HEIGHT];
         frame = new JFrame("Game of Life");
+        tickBegan = false;
 
         System.out.println("Welcome to the Game of Life!");
         inputLiveCells();
@@ -39,28 +42,36 @@ public class Life {
         int neighbors = 0;
 
         if (x > 0 && y > 0) {
-            if (get(x - 1, y - 1)) neighbors ++;
+            if (get(x - 1, y - 1))
+                neighbors++;
         }
         if (y > 0) {
-            if (get(x, y - 1)) neighbors ++;
+            if (get(x, y - 1))
+                neighbors++;
         }
         if (x < WIDTH - 1 && y > 0) {
-            if (get(x + 1, y - 1)) neighbors ++;
+            if (get(x + 1, y - 1))
+                neighbors++;
         }
         if (x > 0) {
-            if (get(x - 1, y)) neighbors ++;
+            if (get(x - 1, y))
+                neighbors++;
         }
         if (x < WIDTH - 1) {
-            if (get(x + 1, y)) neighbors ++;
+            if (get(x + 1, y))
+                neighbors++;
         }
         if (x > 0 && y < HEIGHT - 1) {
-            if (get(x - 1, y + 1)) neighbors ++;
+            if (get(x - 1, y + 1))
+                neighbors++;
         }
         if (y < HEIGHT - 1) {
-            if (get(x, y + 1)) neighbors ++;
+            if (get(x, y + 1))
+                neighbors++;
         }
         if (x < WIDTH - 1 && y < HEIGHT - 1) {
-            if (get(x + 1, y + 1)) neighbors ++;
+            if (get(x + 1, y + 1))
+                neighbors++;
         }
 
         return neighbors;
@@ -69,19 +80,21 @@ public class Life {
     public static void inputLiveCells() {
         frame.setSize(WIDTH * CUBE_SIZE, HEIGHT * CUBE_SIZE);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setUndecorated(true);
         frame.setVisible(true);
 
         // Let the user click on the board to set live cells
-        frame.addMouseListener(new java.awt.event.MouseAdapter() {
+        frame.getContentPane().addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
                 // If it's a right click, finish
                 if (evt.getButton() == java.awt.event.MouseEvent.BUTTON3) {
-                    tick();
+                    // Tick at 60 FPS
+                    beginTickLoop();
                     return;
                 }
 
-                int x = evt.getX() / CUBE_SIZE;
-                int y = evt.getY() / CUBE_SIZE;
+                int x = parseMouseCoord(evt.getX(), frame.getWidth(), WIDTH);
+                int y = parseMouseCoord(evt.getY(), frame.getHeight(), HEIGHT);
 
                 set(x, y, true);
                 printBoard();
@@ -89,10 +102,35 @@ public class Life {
         });
     }
 
+    public static int parseMouseCoord(int coord, int size, int multiplicity) {
+        float coordFloat = (float) coord;
+        float sizeFloat = (float) size;
+        float multiplicityFloat = (float) multiplicity;
+
+        float resultFloat = coordFloat * multiplicityFloat / sizeFloat;
+        return (int) resultFloat;
+    }
+
+    public static void beginTickLoop() {
+        if (tickBegan) {
+            return;
+        }
+
+        tickBegan = true;
+        // Use a timer to tick every 1/60th of a second
+        javax.swing.Timer timer = new javax.swing.Timer(1000 / 10, new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                tick();
+            }
+        });
+
+        timer.start();
+    }
+
     public static void tick() {
         boolean[] newBoard = new boolean[board.length];
 
-        for (int i = 0; i < board.length; i ++) {
+        for (int i = 0; i < board.length; i++) {
             int x = i % WIDTH;
             int y = i / HEIGHT;
             int neighbors = getNeighbors(x, y);
@@ -111,19 +149,11 @@ public class Life {
         board = newBoard;
 
         printBoard();
-
-        try {
-            Thread.sleep(100);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        tick();
     }
 
     public static void printBoard() {
         // Print the board on the frame
-        for (int i = 0; i < board.length; i ++) {
+        for (int i = 0; i < board.length; i++) {
             int x = i % WIDTH;
             int y = i / HEIGHT;
 
