4c8f58b79c0bf86e6a1a0f7601c4a35958f308b9
z46-dev <evanparker6023@gmail.com>
2023-11-01 13:47:11 -0400

    Add a fun program

---
+++ MathIsCool/MathIsCool.java
@@ -0,0 +1,42 @@
+package MathIsCool;
+
+import java.util.Scanner;
+
+public class MathIsCool {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Input a number and I'll tell you about the number!");
+        int inputNumber = input.nextInt();
+
+        evenOdd(inputNumber);
+        prime(inputNumber);
+
+        input.close();
+    }
+
+    public static void evenOdd(int number) {
+        if (number % 2 == 0) {
+            System.out.println("Your number is even!");
+            return;
+        }
+
+        System.out.println("Your number is odd!");
+    }
+
+    public static void prime(int number) {
+        if (number == 1) {
+            System.out.println("Your number is not prime!");
+            return;
+        }
+
+        for (int i = 2; i < number; i++) {
+            if (number % i == 0) {
+                System.out.println("Your number is not prime!");
+                return;
+            }
+        }
+
+        System.out.println("Your number is prime!");
+    }
+}
