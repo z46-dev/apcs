6d461e949ec29676fbadf9c8bfa1635351ad1106
z46-dev <evanparker6023@gmail.com>
2023-12-01 08:58:52 -0500

    asd

--- Chapter12/Stats2.java
+++ Chapter12/Stats2.java
@@ -3,12 +3,10 @@ import java.util.Scanner;
 
 public class Stats2 {
     public static void main(String[] args) {
-        // Modify the Stats application to allow the user to enter the names and grades of the students. The user should be prompted for the name of the file to create and for the number of student grades that will be entered. After the data has been entered and the written to a file, the file should be read and the lowest, highest, and average score displayed.
-
         Scanner input = new Scanner(System.in);
 
         System.out.print("Enter the name of the file to create: ");
-        String fileName = "\\fluorine\\2024\\HOME\\20240629\\CompSci\\Chapter12\\data\\" + input.nextLine();
+        String fileName = "\\\\fluorine\\2024\\HOME\\20240629\\CompSci\\Chapter12\\data\\" + input.nextLine();
 
         System.out.print("Enter the number of students: ");
         int numStudents = input.nextInt();
@@ -25,18 +23,33 @@ public class Stats2 {
             scores[i] = new StatsScore(name, score);
         }
 
+        File file = new File(fileName);
+        if (!file.exists()) {
+            try {
+                file.createNewFile();
+            } catch (IOException ex) {
+                System.out.println("Error creating file!");
+                System.out.println(ex.getMessage());
+                System.exit(1);
+            }
+        }
+
         try {
             FileWriter output = new FileWriter(fileName);
             BufferedWriter writer = new BufferedWriter(output);
 
             for (StatsScore score : scores) {
-                writer.write(score.name + "\n");
-                writer.write(score.score + "\n");
+                writer.write(score.name);
+                writer.newLine();
+                writer.write(String.valueOf(score.score));
+                writer.newLine();
             }
 
             writer.close();
+            output.close();
         } catch (IOException ex) {
             System.out.println("Error writing to file!");
+            System.out.println(ex.getMessage());
             System.exit(4);
         }
     }
