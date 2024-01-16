78cd35780df108b6086ba50b0bc9a9d63c97dde9
z46-dev <45317380+z46-dev@users.noreply.github.com>
2024-01-16 18:24:11 -0500

    Update user authentication logic

--- Midterm/Midterm.java
+++ Midterm/Midterm.java
@@ -2,7 +2,6 @@ package Midterm;
 
 import javax.swing.*;
 import java.awt.*;
-import java.util.ArrayList;
 
 class DrawInstructions {
     int[] XPoints;
@@ -50,15 +49,16 @@ class Item {
 class Room {
     public int x, y;
     public Item item; // why tf doesn't java support native direct pointer manipulation???
-    public boolean isPit;
+    public boolean isPit, isDragon;
 }
 
 class Player {
     public Room room;
-    public ArrayList<Item> inventory;
+    public boolean hasRope, hasArrow;
 
     public Player() {
-        inventory = new ArrayList<>();
+        hasRope = false;
+        hasArrow = false;
     }
 }
 
@@ -66,9 +66,22 @@ public class Midterm {
     public static JFrame frame;
     public static Graphics2D g;
     public static Room[] rooms;
+    public static Player player;
 
     // Get room by coordinates
     public static Room getRoom(int x, int y) {
+        if (x == -1) {
+            x = 9;
+        } else if (x == 10) {
+            x = 0;
+        }
+
+        if (y == -1) {
+            y = 9;
+        } else if (y == 10) {
+            y = 0;
+        }
+
         return rooms[x + y * 10];
     }
 
@@ -78,7 +91,7 @@ public class Midterm {
 
     public static void init() {
         frame = new JFrame("Midterm");
-        frame.setSize(500, 500);
+        frame.setSize(500, 650);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setUndecorated(true);
         frame.setVisible(true);
@@ -108,12 +121,100 @@ public class Midterm {
         rope.value = 10;
         rope.weight = 5;
 
-        placeItem(arrow);
-        placeItem(rope);
+        findValidPos().item = arrow;
+        findValidPos().isDragon = true;
 
-        frame.getContentPane().addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseClicked(java.awt.event.MouseEvent evt) {
-                g = (Graphics2D) frame.getGraphics();
+        player = new Player();
+        player.hasRope = true;
+        player.room = findValidPos();
+
+        // Wait 1s, then paint
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        g = (Graphics2D) frame.getGraphics();
+        paint();
+
+        // Bind WASD to movement
+        frame.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyPressed(java.awt.event.KeyEvent evt) {
+                int x = player.room.x;
+                int y = player.room.y;
+
+                switch (evt.getKeyCode()) {
+                    case java.awt.event.KeyEvent.VK_W:
+                        y--;
+                        break;
+                    case java.awt.event.KeyEvent.VK_A:
+                        x--;
+                        break;
+                    case java.awt.event.KeyEvent.VK_S:
+                        y++;
+                        break;
+                    case java.awt.event.KeyEvent.VK_D:
+                        x++;
+                        break;
+                    case java.awt.event.KeyEvent.VK_F:
+                        if (player.hasArrow) {
+                            player.hasArrow = false;
+
+                            if (getRoom(x - 1, y).isDragon || getRoom(x + 1, y).isDragon || getRoom(x, y - 1).isDragon || getRoom(x, y + 1).isDragon) {
+                                JOptionPane.showMessageDialog(frame, "You killed the dragon!", "You win!", JOptionPane.INFORMATION_MESSAGE);
+                                System.exit(0);
+                            } else {
+                                JOptionPane.showMessageDialog(frame, "You missed!", "You lose!", JOptionPane.INFORMATION_MESSAGE);
+                                System.exit(0);
+                            }
+                        }
+                        break;
+                }
+
+                player.room = getRoom(x, y);
+
+                if (player.room.item != null) {
+                    switch (player.room.item.name) {
+                        case "Arrow":
+                            player.hasArrow = true;
+                            break;
+                        case "Rope":
+                            player.hasRope = true;
+                            break;
+                    }
+
+                    player.room.item = null;
+                }
+
+                if (player.room.isPit) {
+                    if (player.hasRope) {
+                        player.hasRope = false;
+
+                        // Move the player to a nearby non-pit room
+                        if (!getRoom(x - 1, y).isPit) {
+                            player.room = getRoom(x - 1, y);
+                        } else if (!getRoom(x + 1, y).isPit) {
+                            player.room = getRoom(x + 1, y);
+                        } else if (!getRoom(x, y - 1).isPit) {
+                            player.room = getRoom(x, y - 1);
+                        } else if (!getRoom(x, y + 1).isPit) {
+                            player.room = getRoom(x, y + 1);
+                        }
+
+                        JOptionPane.showMessageDialog(frame, "You fell into a pit, but you had a rope!");
+                        findValidPos().item = rope;
+                    } else {
+                        JOptionPane.showMessageDialog(frame, "You fell into a pit!", "You lose!", JOptionPane.INFORMATION_MESSAGE);
+                        System.exit(0);
+                    }
+                }
+
+                if (player.room.isDragon) {
+                    JOptionPane.showMessageDialog(frame, "You were eaten by the dragon!", "You lose!", JOptionPane.INFORMATION_MESSAGE);
+                    System.exit(0);
+                }
+                
                 paint();
             }
         });
@@ -151,25 +252,24 @@ public class Midterm {
         }
     }
 
-    public static void placeItem(Item item) {
+    public static Room findValidPos() {
         for (;;) {
             int x = (int) (Math.random() * 10);
             int y = (int) (Math.random() * 10);
             int pos = x + y * 10;
 
-            if (rooms[pos].isPit || rooms[pos].item != null) {
+            if (rooms[pos].isPit || rooms[pos].item != null || rooms[pos].isDragon || (player != null && rooms[pos] == player.room)) {
                 continue;
             }
 
-            rooms[pos].item = item;
-            break;
+            return rooms[pos];
         }
     }
 
     public static void paint() {
         // Clear the screen
         g.setColor(Color.WHITE);
-        g.fillRect(0, 0, 500, 500);
+        g.fillRect(0, 0, 500, 650);
 
         // Draw the rooms & items
         for (int i = 0; i < 100; i++) {
@@ -186,12 +286,22 @@ public class Midterm {
         }
 
         // Draw the pits
-        g.setColor(Color.RED);
+        g.setColor(Color.GRAY);
         for (int i = 0; i < 100; i++) {
             int x = i % 10;
             int y = i / 10;
             if (rooms[i].isPit) {
                 g.fillRect(x * 50, y * 50, 50, 50);
+            }
+        }
+
+        // Draw the dragon
+        g.setColor(Color.RED);
+        for (int i = 0; i < 100; i++) {
+            int x = i % 10;
+            int y = i / 10;
+            if (rooms[i].isDragon) {
+                g.fillOval(x * 50, y * 50, 50, 50);
             }
         }
 
@@ -203,5 +313,104 @@ public class Midterm {
             g.drawLine(i * 50, 0, i * 50, 500);
             g.drawLine(0, i * 50, 500, i * 50);
         }
+
+        // Draw the player
+        g.setColor(Color.BLUE);
+        g.fillRect(player.room.x * 50, player.room.y * 50, 50, 50);
+
+        // If player is near pit, display warning
+        if (getRoom(player.room.x - 1, player.room.y).isPit || getRoom(player.room.x + 1, player.room.y).isPit || getRoom(player.room.x, player.room.y - 1).isPit || getRoom(player.room.x, player.room.y + 1).isPit) {
+            g.setColor(Color.RED);
+            g.setFont(new Font("Arial", Font.PLAIN, 15));
+            
+            FontMetrics metrics = g.getFontMetrics(g.getFont());
+            int y = 550 - metrics.getHeight() / 2 + metrics.getAscent();
+
+            g.drawString("PIT NEARBY", 5, y);
+        }
+
+        if (player.hasArrow) {
+            g.setColor(Color.BLACK);
+            g.setFont(new Font("Arial", Font.PLAIN, 15));
+            
+            FontMetrics metrics = g.getFontMetrics(g.getFont());
+            int y = 575 - metrics.getHeight() / 2 + metrics.getAscent();
+
+            g.drawString("You have an arrow, press F to fire!", 5, y);
+        } else if (
+            (getRoom(player.room.x - 1, player.room.y).item != null && getRoom(player.room.x - 1, player.room.y).item.name.equals("Arrow")) ||
+            (getRoom(player.room.x + 1, player.room.y).item != null && getRoom(player.room.x + 1, player.room.y).item.name.equals("Arrow")) ||
+            (getRoom(player.room.x, player.room.y - 1).item != null && getRoom(player.room.x, player.room.y - 1).item.name.equals("Arrow")) ||
+            (getRoom(player.room.x, player.room.y + 1).item != null && getRoom(player.room.x, player.room.y + 1).item.name.equals("Arrow"))
+        ) {
+            g.setColor(Color.BLACK);
+            g.setFont(new Font("Arial", Font.PLAIN, 15));
+            
+            FontMetrics metrics = g.getFontMetrics(g.getFont());
+            int y = 575 - metrics.getHeight() / 2 + metrics.getAscent();
+
+            g.drawString("There's an arrow somewhere around!", 5, y);
+        }
+
+        if (player.hasRope) {
+            g.setColor(Color.BLACK);
+            g.setFont(new Font("Arial", Font.PLAIN, 15));
+            
+            FontMetrics metrics = g.getFontMetrics(g.getFont());
+            int y = 600 - metrics.getHeight() / 2 + metrics.getAscent();
+
+            g.drawString("You have a rope!", 5, y);
+        } else if (
+            (getRoom(player.room.x - 1, player.room.y).item != null && getRoom(player.room.x - 1, player.room.y).item.name.equals("Rope")) ||
+            (getRoom(player.room.x + 1, player.room.y).item != null && getRoom(player.room.x + 1, player.room.y).item.name.equals("Rope")) ||
+            (getRoom(player.room.x, player.room.y - 1).item != null && getRoom(player.room.x, player.room.y - 1).item.name.equals("Rope")) ||
+            (getRoom(player.room.x, player.room.y + 1).item != null && getRoom(player.room.x, player.room.y + 1).item.name.equals("Rope"))
+        ) {
+            g.setColor(Color.BLACK);
+            g.setFont(new Font("Arial", Font.PLAIN, 15));
+            
+            FontMetrics metrics = g.getFontMetrics(g.getFont());
+            int y = 600 - metrics.getHeight() / 2 + metrics.getAscent();
+
+            g.drawString("There's a rope somewhere around!", 5, y);
+        }
+
+        // If player is near dragon, display warning
+        if (getRoom(player.room.x - 1, player.room.y).isDragon || getRoom(player.room.x + 1, player.room.y).isDragon || getRoom(player.room.x, player.room.y - 1).isDragon || getRoom(player.room.x, player.room.y + 1).isDragon) {
+            g.setColor(Color.RED);
+            g.setFont(new Font("Arial", Font.PLAIN, 15));
+            
+            FontMetrics metrics = g.getFontMetrics(g.getFont());
+            int y = 550 - metrics.getHeight() / 2 + metrics.getAscent();
+
+            g.drawString("DRAGON NEARBY", 200, y);
+        }
+
+        // Colour key 
+        g.setColor(Color.BLACK);
+        g.setFont(new Font("Arial", Font.PLAIN, 15));
+
+        FontMetrics metrics = g.getFontMetrics(g.getFont());
+        int y = 635 - 15 - metrics.getHeight() / 2 + metrics.getAscent();
+
+        g.drawString("Colour key:", 5, y);
+
+        g.setColor(Color.BLUE);
+        g.fillRect(100, 635 - 15, 15, 15);
+
+        g.setColor(Color.RED);
+        g.fillRect(200, 635 - 15, 15, 15);
+
+        g.setColor(Color.GRAY);
+        g.fillRect(300, 635 - 15, 15, 15);
+
+        g.setColor(Color.BLACK);
+        g.drawString("Player", 120, y);
+
+        g.setColor(Color.RED);
+        g.drawString("Dragon", 220, y);
+
+        g.setColor(Color.GRAY);
+        g.drawString("Pit    (Use WASD to move!)", 320, y);
     }
 }
